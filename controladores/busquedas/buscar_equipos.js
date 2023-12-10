f26111ca84c629ab52ede60354a3880b1f86ebc2
Maria Lujan <marialujan996@hotmail.com>
2023-12-10 02:48:59 -0300

    update solicitudes

---
+++ controladores/busquedas/buscar_equipos.js
@@ -0,0 +1,61 @@
+async function buscarEquipos(req, res, db) {
+    const data = req.params;
+    console.log(req.params)
+    const id_jugador = parseInt(data.id_jugador);
+
+    console.log("jugador: " + id_jugador)
+    // Obtener la lista de equipos
+    const equipos = await db.from('equipo').select('*');
+
+    // Crear un array de promesas para obtener las solicitudes para cada equipo
+    const solicitudesPromises = equipos.data.map(async equipo => {
+        // Obtener la solicitud para el jugador y el equipo actual
+        const solicitud = await db.from('solicitudes')
+            .select('*')
+            .eq('id_jugador', id_jugador)
+            .eq('id_equipo', equipo.id_equipo)
+            .single();
+
+        // Retornar la información del equipo y la solicitud
+        return {
+            ...equipo,
+            cant_jugadores: equipo.id_jugadores.length,
+            estado: estadoJugadorAEquipo(id_jugador, equipo, solicitud.data),
+            solicitud: solicitudJugadorAEquipo(id_jugador, equipo, solicitud.data)
+        };
+    });
+
+    // Esperar a que todas las promesas se resuelvan
+    const equiposTransformados = await Promise.all(solicitudesPromises);
+
+    return res.json(equiposTransformados);
+}
+
+ function solicitudJugadorAEquipo(id_jugador, equipos, solicitud) {
+    if(equipos.id_jugadores?.includes(id_jugador)) {
+       return "No puedes solicitar unirte"     
+    }
+
+    if(!solicitud?.estado || solicitud?.estado.trim() === "") {
+        return "No enviado"
+    }
+
+    if(solicitud.estado) {
+        return solicitud.estado 
+    }
+
+}
+
+function estadoJugadorAEquipo(id_jugador, equipos, solicitud) {
+    if(equipos.id_jugadores?.includes(id_jugador)) {
+        return "Ya perteneces a este equipo"
+    }
+
+    console.log("solicitud: " + solicitud)
+    
+    return !(!solicitud?.estado || solicitud?.estado.trim() === "") ? solicitud.estado : "No enviado";
+}
+
+module.exports = {
+    buscarEquipos: buscarEquipos
+};

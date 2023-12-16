22445ad3f161fd3a81659e4d9806849366d6266b
marialujan019 <40526424+marialujan019@users.noreply.github.com>
2023-12-16 15:48:19 -0300

    modificacion de enviadas.js (#15)
    Co-authored-by: EmmanuelMartinez11 <emma.nionn@gmail.com>

--- controladores/solicitudes/enviadas.js
+++ controladores/solicitudes/enviadas.js
@@ -1,4 +1,4 @@
-async function solicitudEnviada(req, res, db){
+async function solicitudEnviada(req, res, db) {
     const id_jugador = req.params.id;
 
     console.log(id_jugador)
@@ -6,21 +6,23 @@ async function solicitudEnviada(req, res, db){
     const misSolicitudes = await db.from('solicitudes')
         .select('*')
         .eq('id_jugador', id_jugador)
-    
+
     const equipos = await db.from('equipo').select('id_equipo, nombre_equipo').contains('id_jugadores', [req.params.id])
     const equipoData = equipos.data
 
     const data = misSolicitudes.data
-    const responseEquipo = data.map(dato => ({
-        ...dato,
-        nombre_equipo: equipoData.find(nombre => nombre.id_equipo === dato.id_equipo).nombre_equipo
-      }));
-      
-      const filterEstado = responseEquipo.filter(item => item.estado == "Pendiente")
+    const responseEquipo = data.map(dato => {
+        const equipo = equipoData.find(nombre => nombre.id_equipo === dato.id_equipo);
+        return {
+            ...dato,
+            nombre_equipo: equipo ? equipo.nombre_equipo : 'Equipo no encontrado',
+        };
+    });
+
+    const filterEstado = responseEquipo.filter(item => item.estado == "Pendiente")
 
     return res.json(filterEstado)
 }
-
 
 module.exports = {
     solicitudEnviada: solicitudEnviada
